e93bf42b38ff38040b8b743dc9b3089f38d7f95c
Amirhoseinmohammadi <amirmi951@gmail.com>
2025-10-07 00:31:46 +0330

    added songs

---
+++ data/songs.tsx
@@ -0,0 +1,26 @@
+export const musics = [
+  {
+    title: 'Alda - Gangam Pore',
+    artist: 'Coldplay',
+    imageUrl: '/static/images/alda.jpeg',
+    downloadUrl: '/static/music/Alda - Gangam Pore.mp3',
+    duration: '4:20',
+    progress: 45,
+  },
+  {
+    title: 'Alda - Namak Nadare',
+    artist: 'The Weeknd',
+    imageUrl: '/static/images/alda.jpeg',
+    downloadUrl: '/static/music/Alda - Namak Nadare.mp3',
+    duration: '3:22',
+    progress: 60,
+  },
+  {
+    title: 'Shape of You',
+    artist: 'Ed Sheeran',
+    imageUrl: '/static/images/alda.jpeg',
+    downloadUrl: '/static/music/shape.mp3',
+    duration: '3:54',
+    progress: 30,
+  },
+]
